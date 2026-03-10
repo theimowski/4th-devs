5cab1487dfb6159dcd0336d4667f7c8bdf7edae5
Tomasz Heimowski <tomek.heimowski@gmail.com>
2026-03-10 17:14:14 +0100

    Read candidates

--- _tasks/S01E02/main.js
+++ _tasks/S01E02/main.js
@@ -1,4 +1,27 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { fetchHubFile } from './utils.js';
 
-const filename = 'findhim_locations.json';
-await fetchHubFile(filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Process power plants
+const content = await fetchHubFile('findhim_locations.json');
+const { power_plants: powerPlants } = JSON.parse(content);
+
+console.log('--- Power Plants ---');
+for (const name in powerPlants) {
+  console.log(name);
+}
+
+// Process candidates from S01E01
+const resultsPath = path.join(__dirname, '../S01E01/results.json');
+if (fs.existsSync(resultsPath)) {
+  const resultsContent = fs.readFileSync(resultsPath, 'utf-8');
+  const candidates = JSON.parse(resultsContent);
+
+  console.log('\n--- People from S01E01 ---');
+  for (const person of candidates) {
+    console.log(person.name);
+  }
+}

3f8adb858c5ca7e1a59cb905816bc5ac2ac0371e
Tomasz Heimowski <tomek.heimowski@gmail.com>
2026-03-17 13:07:00 +0100

    Download the file only if it doesn't exist (add if to main.js)

--- _tasks/S02E02/main.js
+++ _tasks/S02E02/main.js
@@ -1,2 +1,8 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { fetchHubFile } from '../utils/utils.js';
-await fetchHubFile('electricity.png', import.meta.dirname);
+
+const filePath = path.join(import.meta.dirname, 'electricity.png');
+if (!fs.existsSync(filePath)) {
+    await fetchHubFile('electricity.png', import.meta.dirname);
+}
